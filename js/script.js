43a3546de593eb34d97afa7be8bfaecfe8b6ad12
Dylandk10 <dylandk10@gmail.com>
2023-08-11 10:33:10 -0400

    fixed the search chips for mobile and added two recent search

--- js/script.js
+++ js/script.js
@@ -11,11 +11,8 @@ $(window).scroll(function() {
 
 
 
-
+//key press for search box
 var input = document.getElementById("heroSearch");
-let resentSearch = [];
-
-// Execute a function when the user presses a key on the keyboard
 input.addEventListener("keypress", function(event) {
   if (event.key === "Enter") {
     event.preventDefault();
@@ -23,21 +20,70 @@ input.addEventListener("keypress", function(event) {
   }
 });
 
+
+//global chip compentents
+const chipContent1 = document.getElementById("chipContent")
+const chipContent2 = document.getElementById("chipContent2")
+const chip1 = document.getElementById("chip1");
+const chip2 = document.getElementById("chip2");
+
+
+//search function to add chips
 document.getElementById("heroBtn").addEventListener("click", function(e) {
   e.preventDefault();
-  resentSearch.push(input.value);
 
-  //create and add chip
-  let div = document.createElement('div');
-  let chip = document.createElement('span');
-  div.classList.add("chip");
-  chip.classList.add("closebtn");
-  div.textContent = resentSearch[resentSearch.length -1].toString();
-  chip.innerHTML = " &times;"
-  chip.onclick = function() { this.parentElement.style.display='none' };
-  div.appendChild(chip);
-  let sib = document.getElementById("searches").nextElementSibling;
-  document.getElementById("searches").insertBefore(div, sib);
-  alert("This is jsut a design website and does not search. Strictly to show off design.");
-  $("#recentSearch").show(1500);
+  if(input.value == "") {
+    return;
+  }
+
+  //display the chips
+  if (chipContent1.textContent == "" && chipContent2.textContent == "") {
+    console.log("case 1");
+    chipContent1.textContent = input.value;
+    $("#chip2").hide();
+    $("#chip1").show(1200);
+
+  } else if(chipContent1.textContent != "" && chipContent2.textContent == ""){
+    console.log("case 2");
+    chipContent2.textContent = chipContent1.textContent;
+    chipContent1.textContent = input.value;
+    $("#chip1").show(1200);
+    $("#chip2").show(1200);
+
+  } else if(chipContent1.textContent == "" && chipContent2.textContent != "") {
+    console.log("case 3");
+    chipContent1.textContent = input.value;
+    $("#chip1").show(1200);
+    //createChip("chip1", "chipContent1", 1);
+
+  } else if(chipContent1.textContent != "" && chipContent2.textContent != "") {
+    console.log("case 4");
+    chipContent2.textContent = chipContent1.textContent;
+    chipContent1.textContent = input.value;
+    $("#chip1").show(1200);
+    $("#chip2").show(1200)
+    //createChip("chip1", "chipContent1", 1);
+    //createChip("chip2", "chipContent2", 2);
+  }
+
+
+
+  //alert show and reset
+  alert("This website is for design purposes only. No backend. Just beautiful design by Dylan.");
+  $("#recentSearch").show(1200);
+  input.value = "";
 });
+
+
+
+function hideChip(number, element) {
+  if(number == 1) {
+    chipContent1.textContent = "";
+  } else if(number == 2) {
+    chipContent2.textContent = "";
+  }
+
+  element.parentElement.style.display='none';
+
+}
+
